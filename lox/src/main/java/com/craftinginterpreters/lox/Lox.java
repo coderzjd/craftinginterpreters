dc6fd47d0ee8c6a3cb6cecde14e74da4858ec4f7
coderzjd <1633107710@qq.com>
2025-11-07 23:29:34 +0800

    6

--- lox/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ lox/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -1,7 +1,44 @@
 package com.craftinginterpreters.lox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Lox {
-    public static void main(String[] args) {
-        System.out.println("hello Lox");
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            System.out.println("Running script: " + args[0]);
+            runFile(args[0]);
+        } else {
+            System.out.println("Running REPL");
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+        }
+    }
+    private static void run(String source) {
+        System.out.println(source);
     }
 }
