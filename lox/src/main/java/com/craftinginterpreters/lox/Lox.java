571b8354814293c8803f59f55624303ac8a7d932
coderzjd <1633107710@qq.com>
2025-11-10 17:02:06 +0800

    Interpreter

--- lox/src/main/java/com/craftinginterpreters/lox/Lox.java
+++ lox/src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -30,6 +32,10 @@ public class Lox {
         if (hadError) {
             System.exit(65);
         }
+        if (hadRuntimeError) {
+            System.exit(70);
+        }
+
     }
 
     private static void runPrompt() throws IOException {
@@ -49,18 +55,19 @@ public class Lox {
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        Parser parser = new Parser(tokens);
-        Expr expression = parser.parse();
-
-        // Stop if there was a syntax error.
-        if (hadError)
-            return;
-        System.out.print("expression is ");
-        System.out.println(new AstPrinter().print(expression));
         // For now, just print the tokens.
         for (Token token : tokens) {
             System.out.println(token);
         }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+        // Stop if there was a syntax error.
+        if (hadError) {
+            return;
+        }
+        System.out.print("expression is ");
+        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -79,5 +86,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 + [原始文档](https://craftinginterpreters.com/dedication.html)
 + [翻译文档](https://readonly.link/books/https://raw.githubusercontent.com/GuoYaxiang/craftinginterpreters_zh/main/book.json/-/3.Lox%E8%AF%AD%E8%A8%80.md#34-expressions)
 
-https://readonly.link/books/https://raw.githubusercontent.com/GuoYaxiang/craftinginterpreters_zh/main/book.json/-/4.%E6%89%AB%E6%8F%8F.md#461-%E5%AD%97%E7%AC%A6%E4%B8%B2%E5%AD%97%E9%9D%A2%E9%87%8F
+https://readonly.link/books/https://raw.githubusercontent.com/GuoYaxiang/craftinginterpreters_zh/main/book.json/-/8.%E8%A1%A8%E8%BE%BE%E5%BC%8F%E5%92%8C%E7%8A%B6%E6%80%81.md
