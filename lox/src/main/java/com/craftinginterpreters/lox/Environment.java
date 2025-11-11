cf06dd8ec9199d080ece09d41e305717191cd7d3
coderzjd <1633107710@qq.com>
2025-11-11 22:24:27 +0800

    impl scoped

--- lox/src/main/java/com/craftinginterpreters/lox/Environment.java
+++ lox/src/main/java/com/craftinginterpreters/lox/Environment.java
@@ -4,13 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    // 在每个环境中添加一个对其外围环境的引用
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    // 无参构造函数用于全局作用域环境，它是环境链的结束点
+    Environment() {
+        enclosing = null;
+    }
+
+    // 用来创建一个嵌套在给定外部作用域内的新的局部作用域
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
+        if (enclosing != null) {
+            return enclosing.get(name);
+        }
+        throw new RuntimeError(name,
+                "Undefined variable '" + name.lexeme + "'.");
+    }
 
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme)) {
+            values.put(name.lexeme, value);
+            return;
+        }
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
