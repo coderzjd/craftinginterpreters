67ec701f96c951a7e173f5fa22a9891e31d55d89
coderzjd <1633107710@qq.com>
2025-11-13 21:14:22 +0800

    impl class init construct func

--- lox/src/main/java/com/craftinginterpreters/lox/LoxClass.java
+++ lox/src/main/java/com/craftinginterpreters/lox/LoxClass.java
@@ -27,22 +27,31 @@ class LoxClass implements LoxCallable {
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
         LoxInstance instance = new LoxInstance(this);
+        // 拿到初始化函数
+        LoxFunction initializer = findMethod("init");
+        if (initializer != null) {
+            initializer.bind(instance).call(interpreter, arguments);
+        }
         return instance;
     }
 
     @Override
     public int arity() {
-        return 0;
+        LoxFunction initializer = findMethod("init");
+        if (initializer == null) {
+            return 0;
+        }
+        return initializer.arity();
     }
 }
 // LoxClass 与 LoxInstance 梳理
-// 1、LoxClass 类本身存储了name和 methods 
-    // class Bacon {
-    //   eat() {
-    //     print "Crunch crunch crunch!";
-    //   }
-    // }
-    // 获得一个class : {name: Bacon, methods:[eat] }
+// 1、LoxClass 类本身存储了name和 methods
+// class Bacon {
+//  eat() {
+//      print "Crunch crunch crunch!";
+//  }
+// }
+// 获得一个class : {name: Bacon, methods:[eat] }
 // 2、LoxClass实现了LoxCallable 可以直接 call方法实例化LoxInstance。
-    // Bacon() 
-    // 获得一个Bacon实例：{klass: Bacon,fields:[存储setter的字段]}
+// Bacon()
+// 获得一个Bacon实例：{klass: Bacon,fields:[存储setter的字段]}
