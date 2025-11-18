98ac923c68f01e0e287fdc3cb96929be4375354c
coderzjd <1633107710@qq.com>
2025-11-18 14:29:06 +0800

    feat(clox): add lox.lox and makefile repl

--- clox/compiler.c
+++ clox/compiler.c
@@ -20,6 +20,15 @@ void compile(const char *source)
         {
             printf("   | ");
         }
+        //  token.type 对应 TokenType 枚举类的int表示
+        //  token.length, token.start 传参给 %.*s 用于截取字符串展示
+        //  把 %.*s 拆开看就明白了：
+        //      一个 * 出现在“精度”位置（点号后面）时，精度值由调用栈再取一个 int 实参；
+        //      随后的 s 正常再取一个 char * 实参。
+        // %s 的完整语法
+        //      %[flags][width][.precision][length-modifier]s
+        //      width —— 总占多少列（不够补空格）
+        //      .precision —— 最多取多少字符（字符串专属含义）
         printf("%2d '%.*s'\n", token.type, token.length, token.start);
 
         if (token.type == TOKEN_EOF)
