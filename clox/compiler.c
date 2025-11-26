d8d6b7a9710d682b6e22e95fab9182f79d14985e
coderzjd <1633107710@qq.com>
2025-11-26 16:19:37 +0800

    feat(clox): impl print

--- clox/compiler.c
+++ clox/compiler.c
@@ -102,6 +102,20 @@ static void consume(TokenType type, const char *message)
 
     errorAtCurrent(message);
 }
+
+static bool check(TokenType type)
+{
+    return parser.current.type == type;
+}
+
+static bool match(TokenType type)
+{
+    if (!check(type))
+        return false;
+    advance();
+    return true;
+}
+
 static void emitByte(uint8_t byte)
 {
     writeChunk(currentChunk(), byte, parser.previous.line);
@@ -144,6 +158,9 @@ static void endCompiler()
 #endif
 }
 static void expression();
+static void statement();
+static void declaration();
+
 static ParseRule *getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
@@ -222,7 +239,7 @@ static void number()
 static void string()
 {
     // “把源码里的字符串字面量复制到堆，做成 ObjString，再当成常量塞进字节码。”
-    emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,parser.previous.length - 2)));
+    emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 static void unary()
 {
@@ -316,6 +333,36 @@ static void expression()
 {
     parsePrecedence(PREC_ASSIGNMENT);
 }
+
+static void expressionStatement()
+{
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+    emitByte(OP_POP);
+}
+
+static void printStatement()
+{
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after value.");
+    emitByte(OP_PRINT);
+}
+
+static void declaration()
+{
+    statement();
+}
+static void statement()
+{
+    if (match(TOKEN_PRINT))
+    {
+        printStatement();
+    }
+    else
+    {
+        expressionStatement();
+    }
+}
 bool compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
@@ -323,8 +370,10 @@ bool compile(const char *source, Chunk *chunk)
     parser.hadError = false;
     parser.panicMode = false;
     advance();
-    expression();
-    consume(TOKEN_EOF, "Expect end of expression.");
+    while (!match(TOKEN_EOF))
+    {
+        declaration();
+    }
     endCompiler();
     return !parser.hadError;
 }
--- clox/vm.c
+++ clox/vm.c
@@ -129,6 +129,9 @@ static InterpretResult run()
         case OP_FALSE:
             push(BOOL_VAL(false));
             break;
+        case OP_POP:
+            pop();
+            break;
         case OP_EQUAL:
         {
             Value b = pop();
@@ -182,10 +185,15 @@ static InterpretResult run()
             }
             push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
-        case OP_RETURN:
+        case OP_PRINT:
         {
             printValue(pop());
             printf("\n");
+            break;
+        }
+        case OP_RETURN:
+        {
+            // Exit interpreter.
             return INTERPRET_OK;
         }
         }
